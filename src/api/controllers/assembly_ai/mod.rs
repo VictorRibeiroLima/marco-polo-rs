a300a078b5b8e95eef42ea69e2333e7dc5c25740
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2023-04-27 18:50:30 -0300

    chore: check vars

--- src/api/controllers/assembly_ai/mod.rs
+++ src/api/controllers/assembly_ai/mod.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Root {
+pub struct RequestBody {
     pub status: String,
     #[serde(rename = "transcript_id")]
     pub transcript_id: String,
@@ -15,7 +15,7 @@ pub fn init_routes(config: &mut web::ServiceConfig) {
 }
 
 #[actix_web::post("/transcriptions/webhook")]
-async fn webhook(req_body: web::Json<Root>) -> impl actix_web::Responder {
+async fn webhook(req_body: web::Json<RequestBody>) -> impl actix_web::Responder {
     let req_body = req_body.into_inner();
     let url = format!(
         "https://api.assemblyai.com/v2/transcript/{}/srt",
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,26 @@ mod api;
 mod internals;
 mod queue;
 
+fn check_envs() {
+    //API
+    std::env::var("API_URL").expect("API_URL not found");
+
+    //AWS
+    std::env::var("AWS_BUCKET_NAME").expect("AWS_BUCKET_NAME not found");
+    std::env::var("AWS_ACCESS_KEY_ID").expect("AWS_ACCESS_KEY_ID not found");
+    std::env::var("AWS_SECRET_ACCESS_KEY").expect("AWS_SECRET_ACCESS_KEY not found");
+    std::env::var("AWS_QUEUE_URL").expect("AWS_QUEUE_URL not found");
+
+    //ASSEMBLY_AI
+    std::env::var("ASSEMBLY_AI_API_KEY").expect("ASSEMBLY_AI_API_KEY not found");
+    std::env::var("ASSEMBLY_AI_BASE_URL").expect("ASSEMBLY_AI_BASE_URL not found");
+    std::env::var("ASSEMBLY_AI_WEBHOOK_ENDPOINT").expect("ASSEMBLY_AI_WEBHOOK_ENDPOINT not found");
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv::from_filename(".env").expect("Failed to load .env file");
+    check_envs();
     env_logger::init();
     thread::spawn(queue::init);
     api::init().await
