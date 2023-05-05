38544b72a3d9f26fe29b5f97e78ce452d9239fc6
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2023-05-04 21:01:44 -0300

    chore: set description and title to not null

--- migrations/20230427223753_videos.up.sql
+++ migrations/20230427223753_videos.up.sql
@@ -1,8 +1,8 @@
 -- Add up migration script here
 CREATE TABLE IF NOT EXISTS videos (
   id uuid PRIMARY KEY,
-  title varchar(255),
-  description text,
+  title varchar(255) NOT NULL,
+  description text NOT NULL,
   url varchar(255),
   language varchar(255) NOT NULL DEFAULT 'en',
   created_at timestamp NOT NULL DEFAULT now(),
