15ba80519e78e458d19065db08a1023edb62e8ba
Gustavo Gaiotti <gustavogaiotti2014@gmail.com>
2023-05-19 00:40:38 -0300

    test: test_create_translation

--- core/src/database/queries/translation.rs
+++ core/src/database/queries/translation.rs
@@ -28,3 +28,37 @@ pub async fn create<'a>(
 
     Ok(())
 }
+#[cfg(test)]
+mod test {
+    use std::str::FromStr;
+
+    use sqlx::PgPool;
+    use uuid::Uuid;
+
+    use crate::database::queries::translation::{create, CreateTranslationDto};
+
+    #[sqlx::test(migrations = "../migrations", fixtures("videos", "service_providers"))]
+    async fn test_create_translation(pool: PgPool) {
+        let id = Uuid::from_str("806b57d2-f221-11ed-a05b-0242ac120003").unwrap();
+
+        let dto = CreateTranslationDto {
+            video_id: &id,
+            translator_id: 1234,
+            translation_id: Some(String::from("id_translation")),
+            storage_id: 5678,
+            path: "../translation",
+        };
+
+        create(&pool, dto).await;
+
+        let count = sqlx::query!(
+            "SELECT COUNT(*) AS count FROM videos_translations WHERE video_id = $1",
+            id
+        )
+        .fetch_one(&pool)
+        .await
+        .unwrap();
+
+        assert_eq!(count.count.unwrap(), 1);
+    }
+}
