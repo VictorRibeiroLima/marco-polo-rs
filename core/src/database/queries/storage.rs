ef5775cb384c61f5e8fbc02d68bb33fc38d6ea06
Gustavo Gaiotti <gustavogaiotti2014@gmail.com>
2023-05-21 21:06:08 -0300

    test: test_create_storage_mp4_raw

--- core/src/database/queries/storage.rs
+++ core/src/database/queries/storage.rs
@@ -59,3 +59,37 @@ pub async fn find_by_video_id_and_stage(
 
     Ok(result)
 }
+
+mod test {
+    use std::str::FromStr;
+
+    use sqlx::PgPool;
+
+    use crate::database::models::video_storage::{VideoFormat, VideoStage};
+
+    #[sqlx::test(migrations = "../migrations", fixtures("videos", "service_providers"))]
+    async fn test_create_storage_mp4_raw(pool: PgPool) {
+        let id = uuid::Uuid::from_str("806b57d2-f221-11ed-a05b-0242ac120003").unwrap();
+
+        let dto = super::CreateStorageDto {
+            video_id: &id,
+            video_uri: "https://example.com/video.mp4",
+            storage_id: 1234,
+            format: VideoFormat::Mp4,
+            stage: VideoStage::Raw,
+        };
+
+        let result = super::create(&pool, dto).await;
+        assert!(result.is_ok());
+
+        let count = sqlx::query!(
+            "SELECT COUNT(*) AS count FROM videos_storages WHERE video_id = $1",
+            id
+        )
+        .fetch_one(&pool)
+        .await
+        .unwrap();
+
+        assert_eq!(count.count.unwrap(), 1);
+    }
+}
