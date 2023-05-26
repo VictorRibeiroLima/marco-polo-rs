87da81bcbfdc4acefce0ab28fde1b24bc675da42
Gustavo Gaiotti <gustavogaiotti2014@gmail.com>
2023-05-26 19:35:25 -0300

    test: test_not_found_by_sub_id

--- core/src/database/queries/subtitling.rs
+++ core/src/database/queries/subtitling.rs
@@ -48,4 +48,12 @@ mod test {
 
         assert_eq!(find_success.video_id, id);
     }
+
+    #[sqlx::test(migrations = "../migrations", fixtures("videos_subtitlings"))]
+    async fn test_not_found_by_sub_id(pool: PgPool) {
+        let id = uuid::Uuid::from_str("805b57d2-f221-11ed-a05b-0242ac120003").unwrap();
+        let find_not_success = super::find_by_video_id(&pool, &id).await;
+
+        assert!(find_not_success.is_err());
+    }
 }
