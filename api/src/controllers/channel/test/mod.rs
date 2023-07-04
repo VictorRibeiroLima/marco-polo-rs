f3cd7bd775752e508328d95e5494bead682c0ca7
Gustavo Gaiotti <gustavogaiotti2014@gmail.com>
2023-07-04 20:52:39 -0300

    tests: get channel id

--- api/src/controllers/channel/test/mod.rs
+++ api/src/controllers/channel/test/mod.rs
@@ -4,7 +4,7 @@ use std::sync::Arc;
 
 use actix_http::Request;
 use actix_web::{dev::ServiceResponse, http::header::ContentType, test, web, App};
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, NaiveDate, Utc};
 use marco_polo_rs_core::{
     database::models::user::{User, UserRole},
     internals::youtube_client::{channel_info::ChannelInfo, traits},
@@ -14,8 +14,13 @@ use reqwest::StatusCode;
 use sqlx::PgPool;
 
 use crate::{
-    auth::gen_token, controllers::channel::create_youtube_channel, AppPool, AppYoutubeClient, utils::test::get_token,
+    auth::gen_token,
+    controllers::channel::{create_youtube_channel, dto::ChannelDTO},
+    utils::test::get_token,
+    AppPool, AppYoutubeClient,
 };
+
+use super::find_by_id;
 
 const CSRF_TOKEN: &str = "111aaa11aa";
 
@@ -94,11 +99,100 @@ async fn test_create_channel_authorized(pool: PgPool) {
     assert_eq!(record.count.unwrap(), 1);
 }
 
-#[sqlx::test(migrations = "../migrations", fixtures("user"))]
+#[sqlx::test(migrations = "../migrations", fixtures("user", "channel"))]
+async fn test_find_by_id_get_deleted(pool: PgPool) {
+    let pool = Arc::new(pool);
+
+    let youtube_client = YoutubeClientMock;
+    let youtube_client = Arc::new(youtube_client);
+
+    let token = get_token!(pool.as_ref());
+
+    let test_app = innit_test_app(pool.clone(), youtube_client).await;
+
+    let request = test::TestRequest::get()
+        .uri("/2")
+        .insert_header(ContentType::json())
+        .insert_header(("Authorization", token))
+        .to_request();
+
+    let response = test::call_service(&test_app, request).await;
+    assert_eq!(response.status().as_u16(), StatusCode::NOT_FOUND);
+}
+
+#[sqlx::test(migrations = "../migrations", fixtures("user", "channel"))]
 async fn test_find_by_id_get_ok(pool: PgPool) {
     let pool = Arc::new(pool);
 
+    let youtube_client = YoutubeClientMock;
+    let youtube_client = Arc::new(youtube_client);
+
     let token = get_token!(pool.as_ref());
+
+    let date = NaiveDate::from_ymd_opt(2022, 1, 1)
+        .unwrap()
+        .and_hms_opt(0, 0, 0)
+        .unwrap();
+    let datetime: DateTime<Utc> = DateTime::from_utc(date, Utc);
+
+    let expected_dto: ChannelDTO = ChannelDTO {
+        id: 1,
+        name: Some("Test Channel".to_string()),
+        created_at: datetime,
+        updated_at: datetime,
+    };
+
+    let test_app = innit_test_app(pool.clone(), youtube_client).await;
+
+    let request = test::TestRequest::get()
+        .uri("/1")
+        .insert_header(ContentType::json())
+        .insert_header(("Authorization", token))
+        .to_request();
+
+    let response = test::call_service(&test_app, request).await;
+    assert_eq!(response.status().as_u16(), StatusCode::OK);
+    let actual_dto: ChannelDTO = test::read_body_json(response).await;
+    assert_eq!(actual_dto, expected_dto);
+}
+
+#[sqlx::test(migrations = "../migrations", fixtures("user", "channel"))]
+async fn test_find_by_id_get_not_found(pool: PgPool) {
+    let pool = Arc::new(pool);
+
+    let youtube_client = YoutubeClientMock;
+    let youtube_client = Arc::new(youtube_client);
+
+    let token = get_token!(pool.as_ref());
+
+    let test_app = innit_test_app(pool.clone(), youtube_client).await;
+
+    let request = test::TestRequest::get()
+        .uri("/3")
+        .insert_header(ContentType::json())
+        .insert_header(("Authorization", token))
+        .to_request();
+
+    let response = test::call_service(&test_app, request).await;
+    assert_eq!(response.status().as_u16(), StatusCode::NOT_FOUND);
+}
+
+#[sqlx::test(migrations = "../migrations", fixtures("user", "channel"))]
+async fn test_find_by_id_get_unauthorized(pool: PgPool) {
+    let pool = Arc::new(pool);
+
+    let youtube_client = YoutubeClientMock;
+    let youtube_client = Arc::new(youtube_client);
+
+    let test_app = innit_test_app(pool.clone(), youtube_client).await;
+
+    let request = test::TestRequest::get()
+        .uri("/1")
+        .insert_header(ContentType::json())
+        .to_request();
+
+    let response = test::call_service(&test_app, request).await;
+    assert_eq!(response.status().as_u16(), StatusCode::UNAUTHORIZED);
 }
 
 async fn innit_test_app(
@@ -113,7 +207,8 @@ async fn innit_test_app(
     let app = App::new()
         .app_data(web_data)
         .app_data(web::Data::new(youtube_client))
-        .service(create_youtube_channel);
+        .service(create_youtube_channel)
+        .service(find_by_id);
 
     let test_app = test::init_service(app).await;
 
