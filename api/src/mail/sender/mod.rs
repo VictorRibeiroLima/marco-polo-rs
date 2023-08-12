85d1a43317ff95291548018632ce96c5200cbf5e
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2023-08-11 22:55:18 -0300

    chore: email sender

--- api/src/mail/sender/mod.rs
+++ api/src/mail/sender/mod.rs
@@ -1,1 +1,23 @@
+pub mod lettre;
 
+#[derive(Debug)]
+pub struct MailerError {
+    pub message: String,
+}
+
+pub struct SendEmailOptions {
+    pub to: String,
+    pub subject: String,
+    pub body: String,
+}
+
+impl SendEmailOptions {
+    pub fn new(to: String, subject: String, body: String) -> Self {
+        Self { to, subject, body }
+    }
+}
+
+#[async_trait::async_trait]
+pub trait MailSender {
+    async fn send(&self, options: SendEmailOptions) -> Result<(), MailerError>;
+}
