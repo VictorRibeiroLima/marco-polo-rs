926d788ff01ed31e1c3d7fb6cfd8cbec15fc44a1
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2023-08-24 17:51:29 -0300

    refactor: channel aurh

--- core/src/database/models/channel/auth/data.rs
+++ core/src/database/models/channel/auth/data.rs
@@ -1,17 +1,50 @@
-use super::auth_type::AuthType;
+pub trait AuthData {}
 
-pub trait AuthData {
-    fn get_auth_type() -> AuthType;
-}
-
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, serde::Serialize, serde::Deserialize, Clone, PartialEq)]
 pub struct Oath2Data {
     pub csrf_token: Option<String>,
     pub refresh_token: Option<String>,
 }
 
-impl AuthData for Oath2Data {
-    fn get_auth_type() -> AuthType {
-        AuthType::Oauth2
+#[cfg(test)]
+mod test {
+
+    use serde_json;
+
+    use crate::database::models::channel::auth::AuthType;
+
+    #[test]
+    fn test_deserialize_valid() {
+        let json = r#"{
+      "type": "OAUTH2",
+      "data": {
+        "csrf_token": "csrf_token",
+        "refresh_token": "refresh_token"
+      }
+    }"#;
+
+        let auth: AuthType = serde_json::from_str(json).unwrap();
+
+        match auth {
+            AuthType::Oauth2(data) => {
+                assert_eq!(data.csrf_token, Some("csrf_token".to_string()));
+                assert_eq!(data.refresh_token, Some("refresh_token".to_string()));
+            }
+            _ => {
+                panic!("Invalid auth type")
+            }
+        }
+    }
+
+    #[test]
+    fn test_deserialize_invalid() {
+        let json = r#"{
+      "type": "OAUTH2",
+      "data": "INVALID"
+    }"#;
+
+        let auth: AuthType = serde_json::from_str(json).unwrap();
+
+        assert_eq!(auth, AuthType::Invalid);
     }
 }
--- core/src/database/models/channel/auth/mod.rs
+++ core/src/database/models/channel/auth/mod.rs
@@ -1,40 +1,63 @@
-use sqlx::types::Json;
+use serde_json::Value;
 
-use self::{auth_type::AuthType, data::AuthData};
+use self::data::Oath2Data;
+use serde::{Deserialize, Serialize};
 
-pub mod auth_type;
 pub mod data;
 
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
-pub struct Auth<T: AuthData> {
-    #[serde(rename = "type")] // This is needed because `type` is a reserved keyword in Rust
-    pub auth_type: AuthType,
-    pub data: Json<T>,
+#[derive(Debug, Serialize, Clone, PartialEq)]
+pub enum AuthType {
+    Oauth2(Oath2Data),
+    Invalid,
+}
+
+impl<'de> Deserialize<'de> for AuthType {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let mut value: Value = Deserialize::deserialize(deserializer)?;
+
+        let data = value
+            .get_mut("data")
+            .ok_or(serde::de::Error::missing_field("data"))?
+            .take();
+
+        let type_ = value
+            .get("type")
+            .ok_or(serde::de::Error::missing_field("type"))?;
+
+        match type_.as_str() {
+            Some("OAUTH2") => {
+                let data: Oath2Data = match serde_json::from_value(data) {
+                    Ok(data) => data,
+                    Err(e) => {
+                        eprintln!("Error deserializing OAUTH2 data: {}", e);
+                        return Ok(AuthType::Invalid);
+                    }
+                };
+                Ok(AuthType::Oauth2(data))
+            }
+            _ => {
+                eprintln!("Invalid auth type {:?}", type_);
+                Ok(AuthType::Invalid)
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod test {
 
-    use serde_json;
-
-    use crate::database::models::channel::auth::auth_type::AuthType;
-
-    use super::{data::Oath2Data, Auth};
-
     #[test]
-    fn test_deserialize() {
+    fn test_invalid_input() {
         let json = r#"{
-      "type": "OAUTH2",
-      "data": {
-        "csrf_token": "csrf_token",
-        "refresh_token": "refresh_token"
-      }
-    }"#;
+          "type": "INVALID",
+          "data": {}
+        }"#;
 
-        let auth: Auth<Oath2Data> = serde_json::from_str(json).unwrap();
+        let auth: super::AuthType = serde_json::from_str(json).unwrap();
 
-        assert_eq!(auth.auth_type, AuthType::Oauth2);
-        assert_eq!(auth.data.csrf_token, Some("csrf_token".to_string()));
-        assert_eq!(auth.data.refresh_token, Some("refresh_token".to_string()));
+        assert_eq!(auth, super::AuthType::Invalid);
     }
 }
